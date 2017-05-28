e8274d0a9142ee1586e2e748578f28d0e35367c8
root <root@ocp-master.rhlab.ch>
2017-05-28 15:45:20 +0200

    changed path for persistent storage

--- wp-load.php
+++ wp-load.php
@@ -31,10 +31,10 @@ error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_W
  *
  * If neither set of conditions is true, initiate loading the setup process.
  */
-if ( file_exists( ABSPATH . 'wp-config.php') ) {
+if ( file_exists('/wordpress/wp-config.php') ) {
 
 	/** The config file resides in ABSPATH */
-	require_once( ABSPATH . 'wp-config.php' );
+	require_once('/wordpress/wp-config.php' );
 
 } elseif ( @file_exists( dirname( ABSPATH ) . '/wp-config.php' ) && ! @file_exists( dirname( ABSPATH ) . '/wp-settings.php' ) ) {
 
